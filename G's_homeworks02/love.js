ba05191842443f6ab9cfae7eb61f848df7bb2f9d
mi-chan <watanabe@maki-chan.local>
2015-10-06 23:46:43 +0900

    簡易版μ's相性診断完成

--- G's_homeworks02/love.js
+++ G's_homeworks02/love.js
@@ -1,7 +1,68 @@
 $(function(){
 
-var name = Array();
+var name = new Array();
 
+var HONOKA = [
+  "img/honoka_01.png",
+  "img/honoka_02.png",
+  "img/honoka_03.png"
+];
+
+var UMI = [
+  "img/umi_01.png",
+  "img/umi_02.png",
+  "img/umi_03.png"
+];
+
+var KOTORI = [
+  "img/kotori_01.png",
+  "img/kotori_02.png",
+  "img/kotori_03.png"
+];
+
+var MAKI = [
+  "img/maki_01.png",
+  "img/maki_02.png",
+  "img/maki_03.png"
+];
+
+var HANAYO = [
+  "img/hanayo_01.png",
+  "img/hanayo_02.png",
+  "img/hanayo_03.png"
+];
+
+var RIN = [
+  "img/rin_01.png",
+  "img/rin_02.png",
+  "img/rin_03.png"
+];
+
+var ELI = [
+  "img/eli_01.png",
+  "img/eli_02.png",
+  "img/eli_03.png"
+];
+
+var NOZOMI = [
+  "img/nozomi_01.png",
+  "img/nozomi_02.png",
+  "img/nozomi_03.png"
+];
+
+var NICO = [
+  "img/nico_01.png",
+  "img/nico_02.png",
+  "img/nico_03.png"
+];
+
+$('.bxslider').bxSlider({
+  mode:'fade',
+  speed: 500,
+  auto: true,
+  pause: 5000,
+  captions: true
+});
 
 $("[name=character]").on("click",function(){
   name[0] = $("[name=character]:checked").val();
@@ -44,16 +105,17 @@ $("[name=dislike]").on("click",function(){
 });
 
 $("#button").on("click",function(){
-var member = Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
-/*　member[]  0...ほのか
-              1...うみ
-              2...ことり
-              3...まき
-              4...はなよ
-              5...りん
-              6...えり
-              7...のぞみ
-              8...にこ　*/
+var member = [
+  ["Honoka",0],
+  ["Umi",0],
+  ["Kotori",0],
+  ["Maki",0],
+  ["Hanayo",0],
+  ["Rin",0],
+  ["Eli",0],
+  ["Nozomi",0],
+  ["Nico",0]
+];
 
 for(var i=0; i<6; i++){
 /*　name[]  0...性格
@@ -63,28 +125,87 @@ for(var i=0; i<6; i++){
             4...好き
             5...嫌い　*/
   if(name[i] == "ほのか"){
-    member[0]++;
+    member[0][1]++;
   }else if(name[i] == "うみ"){
-    member[1]++;
+    member[1][1]++;
   }else if(name[i] == "ことり"){
-    member[2]++;
+    member[2][1]++;
   }else if(name[i] == "まき"){
-    member[3]++;
+    member[3][1]++;
   }else if(name[i] == "はなよ"){
-    member[4]++;
+    member[4][1]++;
   }else if(name[i] == "りん"){
-    member[5]++;
+    member[5][1]++;
   }else if(name[i] == "えり"){
-    member[6]++;
+    member[6][1]++;
   }else if(name[i] == "のぞみ"){
-    member[7]++;
+    member[7][1]++;
   }else if(name[i] == "にこ"){
-    member[8]++;
+    member[8][1]++;
   }
 }
-// console.log(member)
+var result = member.sort(function(a,b){return(b[1] - a[1]);});
+console.log(result);
 
-console.log(jQuery.inArray(Math.max.apply(null,member), member));
+if(result[0][1] == result[1][1]){
+  // エラー処理
+  $("#results").html('<img src="img/err.jpg" width = 600px title="エラー"/>');
+  $('#answer p').remove();
+}else if(result[0][0] == "Honoka"){
+  // 画像表示
+  img = $('<img id = "img" width = 600px>').attr('src',HONOKA[Math.floor(Math.random() * HONOKA.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「高坂穂乃果」です。</p>');
+  $('#answer p').css('color','#EF810F');
+}else if(result[0][0] == "Umi"){
+  // 画像表示
+  img = $('<img width = 600px>').attr('src',UMI[Math.floor(Math.random() * UMI.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「園田海未」です。</p>');
+  $('#answer p').css('color','#006FAB');
+}else if(result[0][0] == "Kotori"){
+  // 画像表示
+  img = $('<img width = 600px>').attr('src',KOTORI[Math.floor(Math.random() * KOTORI.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「南ことり」です。</p>');
+  $('#answer p').css('color','#9C9C9C');
+}else if(result[0][0] == "Maki"){
+  // 画像表示
+  img = $('<img width = 600px>').attr('src',MAKI[Math.floor(Math.random() * MAKI.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「西木野真姫」です。</p>');
+  $('#answer p').css('color','#DF3447');
+}else if(result[0][0] == "Hanayo"){
+  // 画像表示
+  img = $('<img width = 600px>').attr('src',HANAYO[Math.floor(Math.random() * HANAYO.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「小泉花陽」です。</p>');
+  $('#answer p').css('color','#97B64D');
+}else if(result[0][0] == "Rin"){
+  // 画像表示
+  img = $('<img width = 600px>').attr('src',RIN[Math.floor(Math.random() * RIN.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「星空凛」です。</p>');
+  $('#answer p').css('color','#F4D500');
+}else if(result[0][0] == "Eli"){
+  // 画像表示
+  img = $('<img width = 600px>').attr('src',ELI[Math.floor(Math.random() * ELI.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「絢瀬絵理」です。</p>');
+  $('#answer p').css('color','#009CD1');
+}else if(result[0][0] == "Nozomi"){
+  // 画像表示
+  img = $('<img width = 600px>').attr('src',NOZOMI[Math.floor(Math.random() * NOZOMI.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「東條希」です。</p>');
+  $('#answer p').css('color','#714C99');
+}else if(result[0][0] == "Nico"){
+  // 画像表示
+  img = $('<img width = 600px>').attr('src',NICO[Math.floor(Math.random() * NICO.length)]);
+  $('#results').html(img);
+  $('#answer').html('<p>あなたにぴったりなのは<br>「矢澤にこ」です。</p>');
+  $('#answer p').css('color','#EE8EA0');
+}
 });
 
 
---
+++ G's_homeworks03/kadai/jan.js
@@ -0,0 +1,22 @@
+$(function(){
+
+  $("[name=ja]").on("click",function(){
+    var name = $(this).val();
+
+console.log(name);
+var bool = Boolean(true);
+
+bool = false;
+console.log(bool);
+});
+
+
+
+
+
+
+
+
+
+
+});
